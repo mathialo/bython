35c66b942c73c368a93102441c8b74fcecca5c33
mathialo <mathias@lohne.nu>
2016-04-06 15:55:19 +0200

    Major changes. Support for multiple files, more reliable flag parser, code cleanup

--- bython.c
+++ bython.c
@@ -1,3 +1,6 @@
+#define VERSON_NUMBER "0.1a"
+#define BUILD_NUMBER 1
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,71 +9,182 @@
 #define false 0
 typedef char bool;
 
-bool ends_in_by(char *str);
-
-
+bool ends_in_by(const char *word);
+void change_file_name(const char *previous_name, char **new_name);
+void print_help();
+void print_version();
+void print_error(int error_number);
+bool parse_flags(const char *flags, bool *compile_only);
 
 int main(int argc, char const *argv[]) {
-	
-	bool compile_only = false;
+	int i;
+	char *new_name;
+	bool cont;
+	bool compile_only;
 	unsigned int filename_position = 1;
+	char *proccmd, *runcmd;
+	int return_val;
 
+	/* Run files by default: */
+	compile_only = false;
+	
+	/* Print args for debug */
+	/*for (i=0; i<argc; i++) {
+		printf("%s\n", argv[i]);
+	}*/
+
+	/* No args, quit program */
 	if (argc <= 1) {
-		printf("No arguments! For help run \"bython -h\"\n");
-		return 0;
-	}
-
-	if (strcmp(argv[1], "-h") == 0) {
-		printf("Bython is a python preprosessor that translates braces into indentation\n");
-		printf("Proper use:\n\n");
-		printf("       bython <opt: flags> [filename] <opt: cmd-args>\n\n");
-		printf("Available flags:\n");
-		printf("    -h   Help. Displays this message\n");
-		printf("    -v   Version. Displays whivh version of bython you have installed\n");
-		printf("    -c   Compile only. Does not run the generated python file.\n");
-		printf("    -a   Compile all .by-files in directory (useful for when you're importing)\n");
-		return 0;
-	}
-
-	if (strcmp(argv[1], "-v") == 0) {
-		printf("Bython v. 0.1a pre-alpha\n");
-		return 0;
-	}
-
-	if (strcmp(argv[1], "-c") == 0) {
-		compile_only = true;
-		filename_position++;
-	}
-
-	char *proccmd = malloc(256*sizeof(char));
-
-	strcat(proccmd, "python /home/mathias/prog/bython/bython.py ");
-	strcat(proccmd, argv[filename_position]);
-
-	int returnVal = system(proccmd);
-
-	if (returnVal != 0) {
-		printf("Bython error: Something went wrong durnig parsing!\n");
+		print_error(0);
 		return -1;
 	}
 
+	/* check for flags, and act accordingly */
+	if (argv[1][0] == '-') {
+		cont = parse_flags(argv[1], &compile_only);
+		filename_position++;
+
+		if (!cont) {
+			return 0;
+		}
+
+		/* We must now update where the file is, and check if we have enough args */
+		if (argc <= 2) {
+			print_error(0);
+			return -1;
+		}
+	}
+
+	/* Process bython-files. Construct command */
+	proccmd = malloc(256*sizeof(char));
+
+	strcat(proccmd, "python /home/mathias/prog/bython/bython.py ");
+
+	for (i=filename_position; i<argc; i++) {
+		strcat(proccmd, " ");
+		strcat(proccmd, argv[i]);
+	}
+
+	/* Run command: */
+	return_val = system(proccmd);
+
+	/* Check for errors during compilation. Exit if there are any */
+	if (return_val != 0) {
+		print_error(1);
+		return -1;
+	}
+
+	/* If the user asked to not run the files, we will stop now. */
 	if (compile_only) {
 		return 0;
 	}
 
-	char *runcmd = malloc(256*sizeof(char));
+	/* Run python-file. Construct command. Change source.by to source.py */
+	change_file_name(argv[1], &new_name);
 
-	strcat(runcmd, "python bython_out.py");
+	runcmd = malloc(256*sizeof(char));
+	strcat(runcmd, "python ");
+	strcat(runcmd, new_name);
 
-	int i;
-	for (i=2; i<argc; i++) {
+	/* cmd-args. deprecated due to compilation of multiple files */
+	/*for (i=2; i<argc; i++) {
 		strcat(runcmd, " ");
 		strcat(runcmd, argv[i]);
-	}
+	}*/
 
 	system(runcmd);
 
-	system("rm bython_out.py");
-
 	return 0;
+}
+
+bool parse_flags(const char *flags, bool *compile_only){
+	int num_of_flags, i;
+
+	num_of_flags = strlen(flags)-1;
+
+	bool result = false;
+
+	for (i=1; i<num_of_flags+1; i++) {
+		switch (flags[i]) {
+			case 'c':
+				*compile_only = true;
+				result = true;
+				break;
+
+			case 'h':
+				print_help();
+				break;
+
+			case 'v':
+				print_version();
+				break;
+
+			default:
+				printf("Doesn't recognize flag: %c\n", flags[i]);
+				exit(0);
+				break;
+
+		}
+	}
+
+	return result;
+}
+
+void print_error(int error_number) {
+	switch (error_number) {
+		case 0:
+			printf("Bython error: No input files! For help, type \"bython -h\"\n");
+			break;
+
+		case 1:
+			printf("Bython error: Something went wrong durnig parsing!\n");
+			break;
+
+	}
+}
+
+void print_help() {
+	printf("Bython is a python preprosessor that translates braces into indentation\n");
+	printf("Proper use:\n\n");
+	printf("       bython <opt: flags> [filename] <opt: cmd-args>\n\n");
+	printf("Available flags:\n");
+	printf("    -h   Help. Displays this message\n");
+	printf("    -v   Version. Displays whivh version of bython you have installed\n");
+	printf("    -c   Compile only. Does not run the generated python file.\n");
+}
+
+
+void print_version() {
+	printf("Bython v%s, build %d\n", VERSON_NUMBER, BUILD_NUMBER);
+	printf("Mathias Lohne, 2016\n");
+}
+
+
+bool ends_in_by(const char *word) {
+	char *tmp = malloc(4*sizeof(char));
+
+	int len = strlen(word);
+
+	strncpy(tmp, word + len-3, 3);
+	tmp[4] = '\0';
+
+	bool result = strcmp(tmp, ".by") == 0;
+	free(tmp);
+
+	return result;
+}
+
+void change_file_name(const char *previous_name, char **new_name) {
+	int len = (int)strlen(previous_name);
+
+	if (ends_in_by(previous_name)) {
+		*new_name = malloc(len*sizeof(char));
+		memcpy(*new_name, previous_name, len);
+		(*new_name)[len-2] = 'p';
+
+	} else {
+		*new_name = malloc((len + 3)*sizeof(char));
+		strcat(*new_name, previous_name);
+		strcat(*new_name, ".py");
+	}
 }
--- bython.py
+++ bython.py
@@ -1,39 +1,58 @@
 import sys
 
-infile = open(sys.argv[1], 'r')
-outfile = open('bython_out.py', 'w')
+def ends_in_by(word):
+	return word[-3:] == ".by"
 
-indentation_level = 0
-indentation_sign = "    "
+def change_file_name(name):
+	if ends_in_by(name):
+		return name[:-3] + ".py"
+	else:
+		return name + ".py"
 
-for line in infile:
-	# skip empty lines:
-	if line in ('\n', '\r\n'):
-		outfile.write(line)
-		continue
+def parse_file(filename):
+	infile = open(filename, 'r')
+	outfile = open(change_file_name(filename), 'w')
 
-	# remove existing whitespace:
-	line = line.lstrip()
+	indentation_level = 0
+	indentation_sign = "    "
 
-	# add new whitespace:
-	for i in range(indentation_level):
-		line = indentation_sign + line
-	
-	# remove brackets and update indentation level
-	line_list = list(line)
+	for line in infile:
+		# skip empty lines:
+		if line in ('\n', '\r\n'):
+			outfile.write(line)
+			continue
 
-	for i in range(len(line_list)):
-		if (line_list[i] == "{"):
-			line_list[i] = ":"
-			indentation_level += 1
+		# remove existing whitespace:
+		line = line.lstrip()
 
-		if (line_list[i] == "}"):
-			line_list[i] = " "
-			indentation_level -= 1
+		# add new whitespace:
+		for i in range(indentation_level):
+			line = indentation_sign + line
+		
+		# remove brackets and update indentation level
+		line_list = list(line)
 
-		if (line_list[i] == ";"):
-			line_list[i] = ""
+		for i in range(len(line_list)):
+			if (line_list[i] == "{"):
+				line_list[i] = ":"
+				indentation_level += 1
 
-	# convert back to string and write line to file
-	line_string = ''.join(line_list)
-	outfile.write(line_string)
+			if (line_list[i] == "}"):
+				line_list[i] = " "
+				indentation_level -= 1
+
+			if (line_list[i] == ";"):
+				line_list[i] = " "
+
+		# convert back to string and write line to file
+		line_string = ''.join(line_list)
+		outfile.write(line_string.rstrip() + "\n")
+
+def main():
+	for i in range(len(sys.argv)):
+		if i==0: continue
+
+		parse_file(sys.argv[i]	)
+
+if __name__ == '__main__':
+	main()
