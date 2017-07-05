70ae52defee1f311d0bb66ddfa1bf4c52cb8cd08
Mathias Lohne <mathialo@student.matnat.uio.no>
2017-07-05 15:20:26 +0200

    renamed py2by to what it actually is

--- src/by2py.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python3
-
-def main():
-	pass
-
-if __name__ == '__main__':
-	main()
---
+++ src/py2by.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+import os
+import re
+import argparse
+
+def main():
+    argparser = argparse.ArgumentParser("py2by", description="Bython is a python preprosessor that translates braces into indentation", formatter_class=argparse.RawTextHelpFormatter)
+	
+
+if __name__ == '__main__':
+	main()
