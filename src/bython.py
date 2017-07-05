0dafda3246348185f9b407e430bb26825721ce3e
Mathias Lohne <mathialo@student.matnat.uio.no>
2017-07-05 10:22:07 +0200

    removed helper obsolete functions

--- src/bython.py
+++ src/bython.py
@@ -2,18 +2,9 @@
 import parser
 import argparse
 
-def ends_in_by(word):
-    return word[-3:] == ".by"
-
-
-def change_file_name(name):
-    if ends_in_by(name):
-        return name[:-3] + ".py"
-    else:
-        return name + ".py"
 
 def main():
-	# Setup argument parser
+    # Setup argument parser
     argparser = argparse.ArgumentParser("bython", description="Bython is a python preprosessor that translates braces into indentation", formatter_class=argparse.RawTextHelpFormatter)
     argparser.add_argument("-v", "--version", action="version", version="Bython v0.3\nMathias Lohne 2017")
     argparser.add_argument("-c", "--compile", help="Compile only (don't run files)", action="store_true")
@@ -27,16 +18,9 @@ def main():
     # Parse arguments
     cmd_args = argparser.parse_args()
 
-    # for i in range(len(sys.argv)):
-    #     if i==0: continue
 
-    #     if i==1 and sys.argv[i] == "ADD_TRUE_LINE":
-    #         add_true_line = True
-    #         continue
-
-    #     parse_file(sys.argv[i], add_true_line)
 
 if __name__ == '__main__':
-	main()
+    main()
 
 
