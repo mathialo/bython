8289869a0b908cf1c140bb4891225a80e1abcdf3
tpepin96 <tpepin96@gmail.com>
2017-07-27 20:57:11 -0400

    py2by almost finished
    Closing braces at end of file are not being recorded

--- src/py2by.py
+++ src/py2by.py
@@ -3,30 +3,17 @@ import os
 import re
 import argparse
 import sys
+from tokenize import tokenize, untokenize, tok_name, INDENT, DEDENT, NAME
 
+'''
+pyfile = open("simpletest.py","rb")
+tokens = list(tokenize(pyfile.readline))
 
-def parse_indent_style_input(indentinput):
-    if not indentinput:
-        return "    "
+pyfile.close()
 
-    elif indentinput[0] == "1s":
-        return " "
-
-    elif indentinput[0] == "2s":
-        return "  "
-
-    elif indentinput[0] == "4s":
-        return "    "
-
-    elif indentinput[0] == "8s":
-        return "        "
-
-    elif indentinput[0] == "t":
-        return "\t"
-
-    else:
-        return None
-
+for token in tokens:
+    print(token.start[0], tok_name[token.exact_type],token.exact_type,token.string)
+'''
 
 def ends_in_py(word):
     return word[-3:] == ".py"
@@ -35,98 +22,75 @@ def ends_in_py(word):
 def change_file_name(name):
     if ends_in_py(name):
         return name[:-3] + ".by"
-
     else:
         return name + ".by"
 
 
-def count_indent_level(whitespace, indent_symbol):
-
-    new_whitespace, numsubs = re.subn(indent_symbol, "", whitespace)
-
-    if not new_whitespace == "":
-        # Not all whitespace was consumed => illigal indentation
-        raise RuntimeError("Illegal indentation detected.\nMaybe the selected indentation style is wrong?")
-
-    else:
-        return numsubs
-
-
-
-def reverse_parse(filename, indent_style):
-    indent_symbol = parse_indent_style_input(indent_style)
-
-    if not indent_symbol:
-        raise RuntimeError("%s is not a valid indentation style!" % (indent_style))
-
-    infile = open(filename, "r")
-    outfile = open(change_file_name(filename), "w")
-
-    last_line = ""
-    last_level = 0
-
-    num_open = 0
-    num_close = 0
-
-    level = 0
-
-    for line in infile:
-        if not line.strip() == "":
-            if line in ("\n", "\n\r", "\r\n"):
-                leading_whitespace = ""
-
-            else:
-                leading_whitespace = line.split(line.lstrip())[0]
-
-            level = count_indent_level(leading_whitespace, indent_symbol)
-
-
-        new_last, num_subs = re.subn(r"\s*:$", " {", last_line)
-        num_open += num_subs
-        outfile.write(new_last)
-
-        if level < last_level:
-            decrese = last_level - level
-            for i in range(decrese):
-                outfile.write((level + decrese - i -1)*indent_symbol + "}\n")
-                num_close += 1
-
-        last_line = line
-        last_level = level
-
-
-    outfile.write(last_line)
-
-    if last_level > 0:
-        outfile.write("\n")
-        decrese = last_level
-        for i in range(decrese):
-            outfile.write((decrese - i - 1)*indent_symbol + "}\n")
-            num_close += 1
-
-
-
-    if num_open > num_close:
-        outfile.write("}\n")
-        num_close += 1
-
-    if (num_open != num_close):
-        raise RuntimeError("Unmatching number of braces created.")
-
+def reverse_parse(filename):
+    infile = open(filename, "rb")
+    
+    inlines = infile.readlines()
+    for index, line in enumerate(inlines):
+        inlines[index] = line.decode("utf-8")
+    infile.seek(0)
+    tokens = list(tokenize(infile.readline))
     infile.close()
-    outfile.close()
 
+    print("Length:",len(tokens))
+    
+    # Stores a list of tuples for INDENT/DEDENT
+    # (token, line number, position in line)
+    indent_tracker = []
+    
+    # Used as stack, to ensure matching braces are on the right level
+    indent_levels = []
+    position = 0;
+    line_of_last_name_token = 0;
+    for token in tokens:
+        current_line = token.start[0]
+        if ((token.exact_type == NAME)
+            and line_of_last_name_token != current_line):
+            line_of_last_name_token = current_line
+            position = token.start[1]
+        if (token.exact_type == INDENT):
+            indent_levels.append(position)
+            indent_tracker.append((INDENT,current_line,position))
+        if (token.exact_type == DEDENT):
+            indent_tracker.append((DEDENT,current_line,indent_levels.pop()))
+            
 
+            
+    # Counts how many extra lines we have
+    # index is our line number
+    for indent in indent_tracker:
+        print(indent)
+    extra = 0
+    
+    for index, line in enumerate(inlines):
+        current = indent_tracker[:1][0]
+        if(index == (current[1] + extra)):   #[:1][0] is first tuple in list
+            #print(index,indent_tracker[:1][0][1],extra)
+            #if (current[0] == INDENT or current[0] == DEDENT):
+            extra += 1
+            inlines.insert(
+                index - 1,
+                current[2]*" "
+                + ("}","{")[current[0] == INDENT]
+            )
+            indent_tracker.pop(0)
+        #print(line)
+
+    for line in inlines:
+        print(line)
 def main():
     argparser = argparse.ArgumentParser("py2by", description="py2by translates python to bython", formatter_class=argparse.RawTextHelpFormatter)
     argparser.add_argument("-v", "--version", action="version", version="py2by is a part of Bython v0.3\nMathias Lohne 2017")
     argparser.add_argument("input", type=str, help="python file to translate", nargs=1)
-    argparser.add_argument("-i", "--indent_style", type=str, help="style of indentation to look for: 1s, 2s, 4s, 8s or t (default: 4s)", nargs=1)
 
     cmd_args = argparser.parse_args()
 
     try:
-        reverse_parse(cmd_args.input[0], cmd_args.indent_style)
+        reverse_parse(cmd_args.input[0])
 
     except RuntimeError as e:
         print("Error: %s" % str(e) , file=sys.stderr)
