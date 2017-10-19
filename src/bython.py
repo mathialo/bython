0d0ed6eeb9229f82280245ef81dfe6dc7cab85de
Mathias Lohne <mathialo@student.matnat.uio.no>
2017-10-19 10:01:02 +0200

    broke stupidly long lines, more PEPy python

--- src/bython.py
+++ src/bython.py
@@ -22,15 +22,35 @@ VERSION_NUMBER = "0.3.2"
 
 def main():
     # Setup argument parser
-    argparser = argparse.ArgumentParser("bython", description="Bython is a python preprosessor that translates braces into indentation", formatter_class=argparse.RawTextHelpFormatter)
-    argparser.add_argument("-v", "--version", action="version", version="Bython v%s\nMathias Lohne and Tristan Pepin 2017" % VERSION_NUMBER)
-    argparser.add_argument("-c", "--compile", help="translate to python only (don't run files)", action="store_true")
-    argparser.add_argument("-m", "--multiple", help="treat arguments as additional files to process", action="store_true")
-    argparser.add_argument("-k", "--keep", help="keep generated python files", action="store_true")
-    argparser.add_argument("-t", "--lower_true", help="adds support for lower case true/false", action="store_true")
-    argparser.add_argument("-2", "--python2", help="use python2 instead of python3 (default)", action="store_true")
-    argparser.add_argument("input", type=str, help="bython files to process", nargs=1)
-    argparser.add_argument("args", type=str, help="arguments to script", nargs=argparse.REMAINDER)
+    argparser = argparse.ArgumentParser("bython", 
+        description="Bython is a python preprosessor that translates braces into indentation", 
+        formatter_class=argparse.RawTextHelpFormatter)
+    argparser.add_argument("-v", "--version", 
+        action="version", 
+        version="Bython v%s\nMathias Lohne and Tristan Pepin 2017" % VERSION_NUMBER)
+    argparser.add_argument("-c", "--compile", 
+        help="translate to python only (don't run files)",
+        action="store_true")
+    argparser.add_argument("-m", "--multiple", 
+        help="treat arguments as additional files to process", 
+        action="store_true")
+    argparser.add_argument("-k", "--keep",
+        help="keep generated python files",
+        action="store_true")
+    argparser.add_argument("-t", "--lower_true",
+        help="adds support for lower case true/false",
+        action="store_true")
+    argparser.add_argument("-2", "--python2",
+        help="use python2 instead of python3 (default)",
+        action="store_true")
+    argparser.add_argument("input",
+        type=str, 
+        help="bython files to process",
+        nargs=1)
+    argparser.add_argument("args",
+        type=str,
+        help="arguments to script",
+        nargs=argparse.REMAINDER)
 
     # Parse arguments
     cmd_args = argparser.parse_args()
@@ -59,7 +79,11 @@ def main():
     else:
         python_command = "python3"
 
-    os.system("%s %s %s" % (python_command, parser._change_file_name(cmd_args.input[0]), " ".join(i for i in cmd_args.args)))
+    os.system("%s %s %s" % (
+        python_command,
+        parser._change_file_name(cmd_args.input[0]),
+        " ".join(arg for arg in cmd_args.args))
+    )
 
     # Delete file if requested
     if not cmd_args.keep:
