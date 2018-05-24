a98cc3b94ddab3da13bd0e0498b8f6bfd0478a97
Mathias Lohne <mathialo@ifi.uio.no>
2018-05-24 10:25:15 +0200

    made ready for PyPI upload

--- Makefile
+++ Makefile
@@ -12,3 +12,18 @@ uninstall:
 	-rm /usr/local/share/man/man1/py2by.1	
 	-rm /usr/local/share/man/man1/by2py.1	
 	mandb > /dev/null
+
+
+# The following part is not depricated. It is used to publish new versions to 
+# PyPI
+
+release: 
+	-rm -r dist
+	python3 setup.py sdist bdist_wheel
+	twine upload dist/*
+
+
+test-release: 
+	-rm -r dist
+	python3 setup.py sdist bdist_wheel
+	twine upload --repository-url https://test.pypi.org/legacy/ dist/*
--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
 from setuptools import setup
 from bython import VERSION_NUMBER
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 # Install python package, scripts and manual pages
 setup(name="bython",
       version=VERSION_NUMBER,
       author="Mathias Lohne",
-      email="mathialo@ifi.uio.no",
+      author_email="mathialo@ifi.uio.no",
       license="MIT",
+      description="Python with braces",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/mathialo/bython",
+      download_url="https://github.com/mathialo/bython/archive/v0.6.tar.gz",
       scripts=["scripts/by2py", "scripts/bython", "scripts/py2by"],
       data_files=[("man/man1", ["etc/bython.1", "etc/py2by.1", "etc/by2py.1"])],
       packages=["bython"],
