b35ca4cfa024f9429a5e6c19d3a73d8a5d24b1cc
Filipe Jordan do Nascimento <filipe.fjn@gmail.com>
2019-05-30 10:42:23 -0300

    Fixed an exception that happened when the file didn't end with a newline

--- bython/parser.py
+++ bython/parser.py
@@ -582,6 +582,9 @@ def parse_file_recursively(filepath, add_true_line=False, filename_prefix="", ou
         filtered_file = open(filename + ".filtered", 'w')
         filtered_file.write(infile_str)
         filtered_file.close()
+    
+    # adding a newline at EOF
+    infile_str += infile_str + "\n"
 
     # start recursive function
     recursive_parser(infile_str, 0, "", outfile, 0, "    ", debug_mode)
